5db5d46f946c0df6903f94bdd502e566e1b29781
Kidan Nelson <nelsonkidan@gmail.com>
2024-11-03 21:43:41 -0500

    fix

--- config/colorschemes/catppuccin.nix
+++ config/colorschemes/catppuccin.nix
@@ -10,7 +10,7 @@
         settings = {
           background = {
             light = "macchiato";
-            dark = "#000";
+            dark = "#000000";
           };
           flavour = "mocha"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
           disable_bold = false;
